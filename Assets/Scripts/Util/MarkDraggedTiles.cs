4255953e4186ef6476dba32002ebab62f92b58a9
David Grimmen <david.grimmen@gmail.com>
2023-07-16 03:16:26 +0200

    Aller Anfang ist schwer

---
+++ Assets/Scripts/Util/MarkDraggedTiles.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Util
+{
+    public class MarkDraggedTiles: MonoBehaviour
+    {
+        [NotNull]
+        private readonly List<Vector3Int> _path = new ();
+    
+        private Camera _camera;
+
+        private int _touchId;
+        private bool _dragging;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Input.touchCount == 0) return;
+
+            var touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (_dragging) break;
+                
+                    _path.Add(ConvertPos(touch.position));
+                    _dragging = true;
+                    break;
+                case TouchPhase.Moved:
+                    if (!_dragging) break;
+                
+                    var pos = ConvertPos(touch.position);
+
+                    if(!_path.Contains(pos))
+                        _path.Add(pos);
+                    break;
+                case TouchPhase.Ended:
+                    if (!_dragging) break;
+
+                    _dragging = false;
+                    _path.Clear();
+                    break;
+                case TouchPhase.Stationary:
+                case TouchPhase.Canceled:
+                default:
+                    break;
+            }
+        }
+
+        private Vector3Int ConvertPos(Vector2 pos)
+        {
+            var world = _camera.ScreenToWorldPoint(pos);
+            return Vector3Int.RoundToInt(world);
+        }
+    }
+}
